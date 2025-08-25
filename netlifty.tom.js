6b876e504a88cf56f2b31c4bbbd0107f721b4ced
Mozola-satlawa <misia.filipek@gmail.com>
2025-08-25 23:13:06 +0200

    Delete netlifty.tom.js

--- netlifty.tom.js
+++
@@ -1,5 +0,0 @@
-export default async (req, context) => {
-  return new Response("Hello from Netlify Function!", {
-    headers: { "Content-Type": "text/plain" },
-  });
-};                                     
