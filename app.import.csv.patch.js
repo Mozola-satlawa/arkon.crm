fc78794ff0e55e615ddede8bafd3bf9b1437b75e
Mozola-satlawa <misia.filipek@gmail.com>
2025-08-28 23:49:47 +0200

    Delete app.import.csv.patch.js

--- app.import.csv.patch.js
+++
@@ -1,9 +0,0 @@
-// app.import.csv.patch.js
-async function importCsv(file){
-  const buf = await file.arrayBuffer();
-  const base64 = btoa(String.fromCharCode(...new Uint8Array(buf)));
-  const res = await fetch('/.netlify/functions/import-csv',{method:'POST',headers:{'Content-Type':'application/json'},body:JSON.stringify({csvBase64:base64})});
-  const data = await res.json();
-  alert('Import zakończony: dodano '+data.inserted+', pominięto '+data.skipped);
-}
-/* W UI: <input type="file" accept=".csv" onchange="importCsv(this.files[0])"> */
